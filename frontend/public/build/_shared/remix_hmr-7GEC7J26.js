2e17ccb428c565b482f6dd5e1fa1e24a0249880b
Ben Fornefeld <50748440+ben-fornefeld@users.noreply.github.com>
2025-06-13 20:16:17 +0200

    remove: unused code

--- frontend/public/build/_shared/remix_hmr-7GEC7J26.js
+++
@@ -1,9 +0,0 @@
-import {
-  createHotContext
-} from "/build/_shared/chunk-L4DLDQUG.js";
-import "/build/_shared/chunk-JR22VO6P.js";
-import "/build/_shared/chunk-PZDJHGND.js";
-export {
-  createHotContext
-};
-//# sourceMappingURL=/build/_shared/remix_hmr-7GEC7J26.js.map
